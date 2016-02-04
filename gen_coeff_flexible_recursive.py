4b56f9cc7d74865ade892f5f9c8f8150c545fc72
danielsf <scott.f.daniel@gmail.com>
2016-02-04 10:09:27 -0800

    only initialize moving objects once
    The first time we loop through the objects, we will store the moving
    objects that we create in a dict that will get referenced later at later
    time steps

--- gen_coeff_flexible_recursive.py
+++ gen_coeff_flexible_recursive.py
@@ -348,6 +348,8 @@ def main(argv):
     if DEBUG:
         print 'datalen', datalen
 
+    moving_object_dict = {}
+
     tmpStartTime = start_time
     while tmpStartTime < start_time + totaldays:
 
@@ -360,13 +362,17 @@ def main(argv):
                     for i in range(datalen):
                         if datalen == 1:
                             id = ssmid
-                            mymo = mo.MovingObject(q, e, inc, omega, argperi, t_p, t_0, objid=ssmid, magHv=H)
+                            if id not in moving_object_dict:
+                                mymo = mo.MovingObject(q, e, inc, omega, argperi, t_p, t_0, objid=ssmid, magHv=H)
+                                moving_object_dict[id] = mymo
                         else:
                             id = ssmid[i]
-                            mymo = mo.MovingObject(q[i], e[i], inc[i], omega[i], argperi[i],
-                                                   t_p[i], t_0[i], objid=ssmid[i], magHv=H[i])
+                            if id not in moving_object_dict:
+                                mymo = mo.MovingObject(q[i], e[i], inc[i], omega[i], argperi[i],
+                                                       t_p[i], t_0[i], objid=ssmid[i], magHv=H[i])
+                                moving_object_dict[id] = mymo
 
-                        doOneMonth(id, mymo, tmpStartTime, days, coeff, multipliers, CoeffFile,
+                        doOneMonth(id, moving_object_dict[id], tmpStartTime, days, coeff, multipliers, CoeffFile,
                                    ResidualSumfile, Failedfile, inputfilename[-1])
 
             tmpStartTime += days
